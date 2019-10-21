1b456b46ad894075f3c3de43f931fce863b898ae
c00lmk <maciek.kasprzak@coolmaciek.pl>
2019-10-21 02:48:32 +0200

    Final stage of initial files setup.

--- public/index.php
+++ public/index.php
@@ -2,26 +2,42 @@
 declare(strict_types=1);
 
 use CERP\HelloWorld;
+use DI\Container;
 use DI\ContainerBuilder;
+use FastRoute\RouteCollector;
+use Middlewares\FastRoute;
+use Middlewares\RequestHandler;
+use Narrowspark\HttpEmitter\SapiEmitter;
 use Relay\Relay;
+use Zend\Diactoros\Response;
 use Zend\Diactoros\ServerRequestFactory;
 use function DI\create;
+use function DI\get;
+use function FastRoute\simpleDispatcher;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $containerBuilder = new ContainerBuilder();
-$containerBuilder->useAutowiring(false);
-$containerBuilder->useAnnotations(false);
 $containerBuilder->addDefinitions([
     HelloWorld::class => create(HelloWorld::class)
+    ->constructor(get('Foo'), get('Response')),
+    'Foo'=>'bar',
+    'Response'=>function(){
+        return new Response();
+    }
 ]);
 
 $container = $containerBuilder->build();
 
-$middlewareQueue = [];
+$routes = simpleDispatcher(function (RouteCollector $r) {
+    $r->get('/hello', HelloWorld::class);
+});
+
+$middlewareQueue[] = new FastRoute($routes);
+$middlewareQueue[] = new RequestHandler($container);
+
 $requestHandler = new Relay($middlewareQueue);
-$requestHandler->handle(ServerRequestFactory::fromGlobals());
+$response = $requestHandler->handle(ServerRequestFactory::fromGlobals());
 
-
-$helloWorld = $container->get(HelloWorld::class);
-$helloWorld->announce();
+$emitter = new SapiEmitter();
+return $emitter->emit($response);
--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -4,11 +4,29 @@
 namespace CERP;
 
 
+use Psr\Http\Message\ResponseInterface;
+
 class HelloWorld
 {
-    public function announce(): void
+    private $foo;
+    private $response;
+
+    public function __construct(string $foo, ResponseInterface $response)
     {
-        echo 'Hello world';
+        $this->foo = $foo;
+        $this->response = $response;
     }
 
+    public function __invoke(): ResponseInterface
+    {
+        $response = $this->response->withHeader('Content-Type', 'text/html');
+        $response->getBody()->write("<html><head></head><body>Hello, {$this->foo} world!</body></html>");
+
+        return $response;
+    }
+
+    public function show(): void
+    {
+        echo "(foo) Hello $this->foo, world" . "\r\n";
+    }
 }
